b3b19fe63d072813c3b5b896984c7a4292afdcae
Lokpati Tiwari <lokpatitiwari9@gmail.com>
2018-08-30 07:16:57 +0000

    completed assignment

--- nptelPython/2-assignment/sorting.py
+++ nptelPython/2-assignment/sorting.py
@@ -5,12 +5,16 @@ def descending(l):
     return True
 
 def valley(l):
+    if(len(l)==1):
+        return False
     for i in range(len(l)-1):
-        #print("in 1st ")
-        if l[i]<l[i+1] and i==len(l)//2:
+        if(l[i]==l[i+1]):
+            return False
+        if l[i]<l[i+1]:
             for j in range(i,len(l)-1):
-                #print("in 2nd ")
-                if l[j]>l[j+1] :
+                if(l[j]==l[j+1]):
+                    return False
+                if l[j]>l[j+1]:
                     return False
             break
         elif i==len(l)-2:
@@ -22,14 +26,10 @@ def transpose(m):
     r=len(m)
     c=len(m[0])
     t=[[0 for x in range(r)] for y in range(c)]
-    print('r={},c={}'.format(r,c))
+
     for i in range(r):
         for j in range(c):
             t[j][i]=m[i][j]
-            print(t)
-        print(t)
+
+
     return t
-
-
-print(transpose([[1,4,9]]))
-
