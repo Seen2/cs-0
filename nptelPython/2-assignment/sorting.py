f085ad86f2050aa3f25a9497937cce211d9c4725
seen2 <tiwarietc@gmail.com>
2018-08-30 09:24:17 +0530

    working on maTRIX

--- nptelPython/2-assignment/sorting.py
+++ nptelPython/2-assignment/sorting.py
@@ -6,11 +6,31 @@ def descending(l):
 
 def valley(l):
     for i in range(len(l)-1):
-        if l[i]<l[i+1]:
-            flag+=1
+        #print("in 1st ")
+        if l[i]<l[i+1] and i==len(l)//2:
+            for j in range(i,len(l)-1):
+                #print("in 2nd ")
+                if l[j]>l[j+1] :
+                    return False
+            break
+        elif i==len(l)-2:
+            return False
+    return True
 
 
+def transpose(m):
+    r=len(m)
+    c=len(m[0]) 
+    t=[[0]*(r-1)]*(c-1)
+    print('r={},c={}'.format(r,c))
+    for i in range(r):
+        for j in range(c):
+            print(i,j)
+            t[j].append(m[i][j])
+    return t
 
+
+print(transpose([[1,4,9]]))
 
 
 
