0c2688d60cefd26feae7f3adce7d6aae6ca72474
seen2 <tiwarietc@gmail.com>
2018-09-15 18:44:10 +0530

    file handling almost done with this

--- nidhi.c
+++ nidhi.c
@@ -7,12 +7,16 @@ int main()
    FILE *fp;
  
    printf("Enter name of a file you wish to see\n");
-   //fgets(file_name,25,stdin);
-   //scanf("%s",&file_name);
-   scanf("%[^\n]%*c", file_name);
+   //In case for taking file name as input
+
+   //fgets(file_name,25,stdin); //method:0 to take string as input but not work here
+   scanf("%s",file_name); //best method:1 to take string as input but work here
+
+   //scanf("%[^\n]%*c", file_name); method:2 to take string as input work here "GOOGLED" (:
  
    fp = fopen(file_name, "r"); // read mode
- 
+
+   //if no file found with name the below condition will execute
    if (fp == NULL)
    {
       perror("Error while opening the file.\n");
@@ -21,10 +25,12 @@ int main()
  
    printf("The contents of %s file are:\n", file_name);
  
+   //take each charector of file and print it continiously unti 
+   //EOF(for all file there is a charector that indicates file ends here) is found
    while((ch = fgetc(fp)) != EOF)
-      printf("%c", ch);
+      printf("%c", ch); //print each charector
  
-   fclose(fp);
+   fclose(fp); //close file to stay away from memory leakage while runing 
    return 0;
 }
 
