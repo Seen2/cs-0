5627caf71ab994074053a642134b962b0e1d5ffc
seen2 <tiwarietc@gmail.com>
2018-09-23 21:41:28 +0530

    nptel assignment 7

--- abhishek.c
+++ abhishek.c
@@ -3,11 +3,39 @@ int find_even(int k);
 
 int main(void)
 {
-	printf("hello\n");
+	int k;
+	scanf("%i",&k);
+	find_even(k);
 }
 
 
-int find_even(int k);
+int find_even(int k)
 {
+	int input=0,i=0,flag=0,s[100];
+	while(input!=-1)
+	{
+		scanf("%i",&input);
+		s[i]=input;
+		i++;
+	}
+	i=0;
+	while(s[i]!=-1)
+	{
+		if(s[i]%2==0)
+		{
+			flag++;
+		}
+		if(flag==k)
+		{
+			printf("%i",s[i]);
+			break;
+		}
+		i++;
+
+	}
+	if(i<k)
+	{
+		printf("%d",s[i]);
+	}
 
 }
