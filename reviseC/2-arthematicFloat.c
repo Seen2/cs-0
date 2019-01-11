ed3971cc5eaeedec9cd97dbe1c2c03527ace3b5a
seen2 <tiwarietc@gmail.com>
2019-01-11 20:18:49 +0530

    arthemetic operator for float value

---
+++ reviseC/2-arthematicFloat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float x,y;
+	printf("x=");
+	scanf("%f",&x);
+	printf("y=");
+	scanf("%f",&y);
+	printf("x+y=%f\n",x+y);
+	printf("x-y=%f\n",x-y);
+	printf("x*y=%f\n",x*y);
+	printf("x/y=%f\n",x/y);
+}
