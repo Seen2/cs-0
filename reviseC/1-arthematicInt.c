b7441b897889cd21a3d93d6b9bbd3b9740bcc3d9
seen2 <tiwarietc@gmail.com>
2019-01-11 19:31:57 +0530

    arthemetic operator for integer value

---
+++ reviseC/1-arthematicInt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x,y;
+	printf("x=");
+	scanf("%i",&x);
+	printf("y=");
+	scanf("%i",&y);
+	printf("x+y=%i\n",x+y);
+	printf("x-y=%i\n",x-y);
+	printf("x*y=%i\n",x*y);
+	printf("x/y=%i\n",x/y);
+	printf("x mod y=%i\n",x%y);
+}
